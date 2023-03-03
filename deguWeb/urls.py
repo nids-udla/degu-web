3bc0a5f4760aadb88656ec7c8a4f6034c960bc75
Franco Marchese <franco.marchese@sulz.cl>
2023-03-03 10:40:13 -0300

    added front end files

--- deguWeb/urls.py
+++ deguWeb/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('download', views.download, name="done"),
     path('extended', views.extended, name="extended"),
+    path('vue', TemplateView.as_view(template_name = 'index.html')),
     ]
